85d2638eeccd12ec6435b4cc58d150741484c354
shahbaz123-gif <sajjadahmadjan30@gmail.com>
2025-08-26 21:53:11 +0500

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,14 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of two sets:")
+setz = setx.intersection(sety)
+print(setz)
+x = {1,2,2,3,4,4,4}
+y = {2,4,4,6}
+print("difference")
+print(x.difference(y))
+print("symmetric difference")
+print(x.symmetric_difference(y))
